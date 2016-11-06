032465d7e8888a8f385916192aac7bf27db39347
han <han@132.69.200.33>
2016-11-06 11:24:06 +0200

    setup structure for importing

--- src/main/java/ac/technion/schemamatching/processinstancematching/XESImporter.java
+++ src/main/java/ac/technion/schemamatching/processinstancematching/XESImporter.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.zip.ZipFile;
 
 import org.deckfour.xes.extension.std.XConceptExtension;
@@ -15,34 +16,74 @@ import org.deckfour.xes.in.XParser;
 import org.deckfour.xes.in.XParserRegistry;
 import org.deckfour.xes.in.XesXmlParser;
 import org.deckfour.xes.model.XLog;
+import org.jbpt.petri.NetSystem;
+import org.jbpt.petri.Transition;
 
+import ac.technion.iem.ontobuilder.core.ontology.Attribute;
+import ac.technion.iem.ontobuilder.core.ontology.Domain;
+import ac.technion.iem.ontobuilder.core.ontology.DomainEntry;
 import ac.technion.iem.ontobuilder.core.ontology.Ontology;
+import ac.technion.iem.ontobuilder.core.ontology.OntologyClass;
+import ac.technion.iem.ontobuilder.core.ontology.Term;
 import ac.technion.iem.ontobuilder.io.imports.ImportException;
 import ac.technion.iem.ontobuilder.io.imports.Importer;
 
 public class XESImporter implements Importer {
 
-
+	private Ontology ontology;
 	
 	
 	@Override
 	public Ontology importFile(File file) throws ImportException {
-		Ontology xesOntology = new Ontology(file.getName());
-		
-		XLog log = importXLog(file);
-		
-		
-		
-		return xesOntology;
+		return importFile(file, null);
 	}
 
 	@Override
-	public Ontology importFile(File arg0, File arg1) throws ImportException, UnsupportedOperationException {
-		// TODO Auto-generated method stub
-		//TODO: what should we do with this?
-		return null;
+	public Ontology importFile(File modelFile, File instanceFile) throws ImportException, UnsupportedOperationException {
+		//TODO: since we only have .XES files, for now these two will be the same
+		//TODO: if they also might not be the same, add some handler for that scenario
+		
+		File file = modelFile;
+		ontology = new Ontology(file.getName().substring(0, file.getName().length()-4));
+		XLog log = importXLog(file);
+		
+		createOntology(log);
+		
+		addInstanceInformation(log);
+		
+		return ontology;
+		
 	}
 
+	private void createOntology(XLog log) {
+		OntologyClass eventClassClass = new OntologyClass("eventClass");
+		ontology.addClass(eventClassClass);
+		
+		for (String eventClass : XESPropertyExtractor.getEventClasses(log)) {
+			Term term = new Term(eventClass, eventClass);
+			ontology.addTerm(term);
+			term.setSuperClass(eventClassClass);
+			for (String attrName : XESPropertyExtractor.getAttributeClasses(log, eventClass)) {
+				Attribute attribute = new Attribute(attrName, attrName);
+				term.addAttribute(attribute);
+				attribute.getDomain().setType(XESPropertyExtractor.getAttributeType(log, eventClass, attrName));
+			}
+		}
+	}
+	
+	private void addInstanceInformation(XLog log) {
+		//Create domains
+		for (Term t : ontology.getTerms(false)) {
+			for (Attribute attr : t.getAttributes()) {
+				Domain d = attr.getDomain();
+				for (Object o : XESPropertyExtractor.getAttributeValues(log, t.getName(), attr.getName())) {
+					d.addEntry(new DomainEntry(d, o));
+				}
+			}
+		}
+	}
+	
+	
 	private XLog importXLog(File file) throws ImportException {
 		InputStream input;
 		try {
@@ -57,6 +98,8 @@ public class XESImporter implements Importer {
 	
 	private XLog importXLogFromStream(InputStream input, String filename, long fileSizeInBytes,
 			XFactory factory) {
+		//this is modified from the ProMImport framework to work without pluginContexts
+	
 		//	System.out.println("Open file");
 		XParser parser;
 		if (filename.toLowerCase().endsWith(".xes") || filename.toLowerCase().endsWith(".xez")
@@ -112,14 +155,6 @@ public class XESImporter implements Importer {
 //		if (log.isEmpty()) {
 //			throw new Exception("No process instances contained in log!");
 //		}
-
-//		/*
-//		 * Set the log name as the name of the provided object.
-//		 */
-//		if (context != null) {
-//			context.getFutureResult(0).setLabel(XConceptExtension.instance().extractName(log));
-//		}
-
 		return log;
 	}
 	
